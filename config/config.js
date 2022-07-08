8b3c8ad8bfa7891caac482a452780ed06b1153dd
calebnjw <lelebng@gmail.com>
2022-07-08 11:13:28 +0800

    modified config.js

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  "development": {
-    "username": "calebnjw",
-    "password": null,
-    "database": "weather_development",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
-  }
-}
+  development: {
+    username: 'calebnjw',
+    password: null,
+    database: 'weather_development',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+};
