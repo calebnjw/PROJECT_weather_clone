009152154d6203b8a57437c68b4c0aaabd61944b
calebnjw <lelebng@gmail.com>
2022-07-08 15:45:55 +0800

    modified config file

--- config/config.js
+++ config/config.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 module.exports = {
   development: {
     username: process.env.DB_USER_NAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DEV_NAME,
-    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD || 'null',
+    database: process.env.DB_DEV_NAME || 'weather_development',
+    host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'postgres',
   },
 };
