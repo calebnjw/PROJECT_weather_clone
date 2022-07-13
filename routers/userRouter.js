586ffc73e04b92469971882ae427ca76dd4a68fb
Vivien Phang <101629147+vivienphang@users.noreply.github.com>
2022-07-13 12:37:53 +0800

    first commit

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -10,12 +10,9 @@ class UserRouter {
   router() {
     // insert routes here
     // have to .bind(this.controller) at the end of each route
-    router.get('/login', this.controller.renderLogin.bind(this.controller));
-    router.get('/signup', this.controller.renderSignup.bind(this.controller));
-
     router.get('/favourites', this.controller.getFavourites.bind(this.controller));
 
-    router.get('/logout', this.controller.logout.bind(this.controller));
+    // router.get('/logout', this.controller.userLogout.bind(this.controller));
 
     router.post('/login', this.controller.userLogin.bind(this.controller));
     router.post('/signup', this.controller.userSignup.bind(this.controller));
--- webpack_conf/webpack.common.js
+++ webpack_conf/webpack.common.js
@@ -24,7 +24,7 @@ module.exports = {
       {
         test: /\.(png|jpg|gif|svg|css|eot|ttf)$/,
         loader: 'url-loader',
-      }
+      },
     ],
   },
 };
