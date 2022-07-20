e0ae90714d889894b3c72a5fc0b5c6f6de072de2
calebnjw <lelebng@gmail.com>
2022-07-20 22:00:13 +0800

    modified user_locations table, removed location-id col, added city, lat and long

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,7 @@
 const { default: axios } = require('axios');
 const bcrypt = require('bcrypt');
-<<<<<<< HEAD
 const jwt = require('jsonwebtoken');
 
-=======
->>>>>>> 02d0be2d832dcba25b2b15729232153d7359c144
 const BaseController = require('./baseController.js');
 
 class UserController extends BaseController {
@@ -14,74 +11,56 @@ class UserController extends BaseController {
   }
 
   async userSignup(req, res) {
-    const { firstName, lastName, username, email, password } = req.body;
+    const {
+      firstName, lastName, username, email, password,
+    } = req.body;
     try {
-      const checkEmail = await this.model.findOne({ where: { email } })
-      console.log('Email:', checkEmail)
-<<<<<<< HEAD
+      const checkEmail = await this.model.findOne({ where: { email } });
+      console.log('Email:', checkEmail);
+
       if (checkEmail) {
-        return res.json({ success: false, message: 'Email in use, try again or login' })
-=======
-      if(checkEmail){
-        return res.json({ message: "Email in use, try another email or login!" })
->>>>>>> 02d0be2d832dcba25b2b15729232153d7359c144
-        // return res.status(400).json({success: false, message: 'Email in use, try again or login'})
+        return res.json({ message: 'Email in use, try another email or login!' });
       }
-      const hashedPassword = await bcrypt.hash(password, Number(process.env.SALT_ROUND))
-      const newUser = await this.model.create({ firstName, lastName, username, email, password: hashedPassword })
-      const payload = { id: newUser.id, username: newUser.username }
-      console.log('This is my payload', payload)
-      const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXP })
-      console.log('This is backend token', token)
-<<<<<<< HEAD
-      return res.json({ newUser, token })
+
+      const hashedPassword = await bcrypt.hash(password, Number(process.env.SALT_ROUND));
+      const newUser = await this.model.create({
+        firstName, lastName, username, email, password: hashedPassword
+      });
+
+      const payload = { id: newUser.id, username: newUser.username };
+      console.log('This is my payload', payload);
+      const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXP });
+      console.log('This is backend token', token);
+
+      return res.json({ newUser, token });
     } catch (err) {
-      console.log(err)
+      res.json({ err: err.message });
     }
-    console.log('This is our userSignup route')
-=======
-      return res.json({newUser, token})
-    } catch(err){
-      res.json({ err: err.message })
-    }
->>>>>>> 02d0be2d832dcba25b2b15729232153d7359c144
   }
 
   async userLogin(req, res) {
-    console.log("I am userLogin route")
+    console.log('I am userLogin route');
     const { username, password } = req.body;
     try {
-<<<<<<< HEAD
-      const user = await this.model.findOne({ where: { username } })
+      const user = await this.model.findOne({ where: { username } });
+      console.log('This is the user', user);
+
       if (!user) {
-        return res.json({ success: false, message: 'User does not exist' })
+        return res.json({ message: 'User does not exist' });
       }
-      const compare = await bcrypt.compare(password, user.password)
+
+      console.log('This is the password', password);
+      const compare = await bcrypt.compare(password, user.password);
       if (!compare) {
-        return res.json({ success: false, message: 'Wrong password, try again!' })
+        return res.json({ message: 'Wrong password, try again!' });
       }
-      const payload = { id: user.id, username: user.username }
-      const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXP })
-      return res.json({ user, token })
+
+      const payload = { id: user.id, username: user.username };
+      const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXP });
+
+      return res.json({ user, token });
     } catch (err) {
-      console.log(err)
-=======
-      const user = await this.model.findOne({where: { username }})
-      console.log('This is the user', user)
-      if(!user){
-        return res.json({ message:'User does not exist'})
-      }
-      const compare = await bcrypt.compare(password, user.password)
-      console.log('This is the password', password)
-      if(!compare){
-        return res.json({ message:'Wrong password, try again!'})
-      }
-      const payload = {id: user.id, username: user.username}
-      const token = jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXP})
-      return res.json({user, token})
-    } catch(err){
-      res.send({ message: "No user found. Sign up!" })
->>>>>>> 02d0be2d832dcba25b2b15729232153d7359c144
+      res.send({ message: 'No user found. Sign up!' });
     }
   }
 
@@ -98,14 +77,6 @@ class UserController extends BaseController {
     }
   }
 
-  async renderLogin(request, response) {
-    console.log('I am render login')
-  }
-
-  async renderSignup(request, response) {
-    console.log('I am render Signup')
-  }
-
   async newFavourite(request, response) {
     const { userId, locationId } = request.body;
 
@@ -117,14 +88,6 @@ class UserController extends BaseController {
     } catch (error) {
       console.log(error);
       response.status(400).send({ error });
-    }
-  }
-
-  logout(request, response) {
-    try {
-      console.log('I have logged out')
-    } catch (err) {
-      console.log(err)
     }
   }
 }
